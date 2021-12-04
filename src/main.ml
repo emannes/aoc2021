a2b4dee07de5d7dc0ab8c260072fd8c5472029ec
Eric Mannes <eric@ericmann.es>
2021-12-04 14:07:37 -0500

    problem 2

--- src/main.ml
+++ src/main.ml
@@ -56,7 +56,107 @@ module Problem_1 : S = struct
   ;;
 end
 
-let problems = [ (module Problem_1 : S) ]
+module Problem_2 : S = struct
+  module Direction = struct
+    module T = struct
+      type t =
+        | Forward
+        | Down
+        | Up
+      [@@deriving sexp]
+    end
+
+    include T
+    include Sexpable.To_stringable (T)
+  end
+
+  module Instruction = struct
+    type t =
+      { direction : Direction.t
+      ; units : int
+      }
+
+    let of_string s =
+      let direction, units = String.rsplit2_exn s ~on:' ' in
+      { direction = Direction.of_string (String.capitalize direction)
+      ; units = Int.of_string units
+      }
+    ;;
+  end
+
+  type t = Instruction.t list
+
+  let parse_input = List.map ~f:Instruction.of_string
+
+  let solve_a instructions =
+    let final_depth =
+      List.sum
+        (module Int)
+        instructions
+        ~f:(fun { Instruction.direction; units } ->
+          match direction with
+          | Down -> units
+          | Up -> -units
+          | Forward -> 0)
+    in
+    let final_horizontal_position =
+      List.sum
+        (module Int)
+        instructions
+        ~f:(fun { direction; units } ->
+          match direction with
+          | Down | Up -> 0
+          | Forward -> units)
+    in
+    final_depth * final_horizontal_position
+  ;;
+
+  module State = struct
+    type t =
+      { horizontal_position : int
+      ; depth : int
+      ; aim : int
+      }
+
+    let init = { horizontal_position = 0; depth = 0; aim = 0 }
+  end
+
+  let solve_b instructions =
+    let { State.horizontal_position; depth; aim = _ } =
+      List.fold
+        instructions
+        ~init:State.init
+        ~f:(fun state { Instruction.direction; units } ->
+          match direction with
+          | Down -> { state with aim = state.aim + units }
+          | Up -> { state with aim = state.aim - units }
+          | Forward ->
+            { state with
+              horizontal_position = state.horizontal_position + units
+            ; depth = state.depth + (state.aim * units)
+            })
+    in
+    horizontal_position * depth
+  ;;
+
+  let%expect_test _ =
+    let input = {|
+  forward 5
+  down 5
+  forward 8
+  up 3
+  down 8
+  forward 2
+  |} in
+    let input = parse_input (clean_input input) in
+    print_s [%message (solve_a input : int)];
+    let%bind () = [%expect {| ("solve_a input" 150) |}] in
+    print_s [%message (solve_b input : int)];
+    [%expect{| ("solve_b input" 900) |}]
+  ;;
+end
+
+let problems = [ (module Problem_1 : S); (module Problem_2 : S) ]
 
 let command =
   Command.async
